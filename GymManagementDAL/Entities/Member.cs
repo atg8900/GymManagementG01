3ca640eada23682b306e382156dfef07f636656c
atg8900 <abdallahtarek888888@gmail.com>
2025-10-08 13:52:33 +0300

    Core Entities

---
+++ GymManagementDAL/Entities/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    class Member:GymUser
+    {
+        //joinDate = CreatedAt
+
+        public string ? Photo { get; set; }
+    }
+}
---
+++ GymManagementDAL/Entities/Plan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Entities
+{
+    class Plan:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DurationDays { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
