65f0e287f8b2cd056dfe3bfbcd7f81d49a4f675f
atg8900 <abdallahtarek888888@gmail.com>
2025-10-11 15:43:05 +0300

    Task

---
+++ GymManagementDAL/Repositories/Interfaces/IPlanRepository.cs
@@ -0,0 +1,17 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    interface IPlanRepository
+    {
+        IEnumerable<Plan> GetAllPlans();
+        //GetById
+        Plan? GetPlanById(int id);
+        int Update(Plan plan);
+    }
+}
---
+++ GymManagementDAL/Repositories/Interfaces/ISessionRepository.cs
@@ -0,0 +1,24 @@
+﻿using GymManagementDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace GymManagementDAL.Repositories.Interfaces
+{
+    interface ISessionRepository
+    {
+        //GetAll
+        IEnumerable<Session> GetAllSessions();
+        //GetById
+        Session? GetSessionById(int id);
+        //Add
+        int Add(Session session);
+        //Update
+        int Update(Session session);
+        //Delete
+        int Remove(int id);
+    }
+}
