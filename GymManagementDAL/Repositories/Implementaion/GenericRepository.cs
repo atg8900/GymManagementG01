ee3cb2d290c1fd3c2b247d8f9a28136c28cd31ed
atg8900 <abdallahtarek888888@gmail.com>
2025-10-11 21:23:54 +0300

    Generic Repository Pattern

---
+++ GymManagementDAL/Repositories/Implementaion/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using GymManagementDAL.Data.Contexts;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementDAL.Repositories.Implementaion
+{
+    class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity:BaseEntity , new()
+    {
+
+        private readonly GymDbContext _dbContext;
+
+        public GenericRepository(GymDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public int Add(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(TEntity entity)
+        {
+
+            _dbContext.Set<TEntity>().Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll() => _dbContext.Set<TEntity>().ToList();
+
+        public TEntity? GetById(int id) => _dbContext.Set<TEntity>().Find(id);
+
+
+        public int Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
