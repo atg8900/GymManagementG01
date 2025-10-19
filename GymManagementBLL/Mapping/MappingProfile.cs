ab84d2d4a914255e90ef6fe55c6e0a8f66c905ed
atg8900 <abdallahtarek888888@gmail.com>
2025-10-19 19:00:06 +0300

    Task

--- GymManagementBLL/Mapping/MappingProfile.cs
+++ GymManagementBLL/Mapping/MappingProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using GymManagementBLL.View_Models;
+using GymManagementBLL.View_Models.Plan_VM;
 using GymManagementBLL.View_Models.Session_VM;
+using GymManagementBLL.View_Models.Trainer_VM;
 using GymManagementDAL.Entities;
 using System;
 using System.Collections.Generic;
@@ -13,13 +16,71 @@ namespace GymManagementBLL.Mapping
     {
         public MappingProfile()
         {
+            #region mapping session
             CreateMap<Session, SessionViewModel>()
-                .ForMember(dest => dest.TrainerName, options =>options.MapFrom(src => src.Trainer.Name))
-                .ForMember(dest => dest.CategoryName, options =>options.MapFrom(src => src.Category.CategoryName))
-                .ForMember(dest => dest.AvailableSlots , options => options.Ignore());
+                    .ForMember(dest => dest.TrainerName, options => options.MapFrom(src => src.Trainer.Name))
+                    .ForMember(dest => dest.CategoryName, options => options.MapFrom(src => src.Category.CategoryName))
+                    .ForMember(dest => dest.AvailableSlots, options => options.Ignore());
 
             CreateMap<CreateSessionViewModel, Session>();
-            CreateMap< Session , UpdateSessionViewModel>().ReverseMap();
+            CreateMap<Session, UpdateSessionViewModel>().ReverseMap();
+            #endregion
+
+            #region member
+            CreateMap<CreateMemberViewModel, Member>()
+                  .ForMember(dest => dest.Address, opt => opt.MapFrom(src => new Address
+                  {
+                      BuildingNumber = src.BuildingNumber,
+                      City = src.City,
+                      Street = src.Street
+                  }));
+            CreateMap<HealthRecordViewModel, HealthRecord>().ReverseMap();
+            CreateMap<Member, MemberViewModel>()
+                .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth.ToShortDateString()))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => $"{src.Address.BuildingNumber}-{src.Address.Street}-{src.Address.City}"));
+
+
+            CreateMap<Member, MemberToUpdateViewModel>()
+                .ForMember(dest => dest.BuildingNumber, opt => opt.MapFrom(src => src.Address.BuildingNumber))
+                .ForMember(dest => dest.Street, opt => opt.MapFrom(src => src.Address.Street))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Address.City));
+
+
+            #endregion
+
+            #region plan
+            CreateMap<Plan, PlanViewModel>();
+
+            CreateMap<Plan, PlanToUpdateViewModel>();
+
+            CreateMap<PlanToUpdateViewModel, Plan>()
+                .ForMember(dest => dest.Name, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+            #endregion
+
+            #region trainer
+
+            CreateMap<CreateTrainerViewModel, Trainer>()
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => new Address
+                {
+                    BuildingNumber = src.BuildingNumber,
+                    Street = src.Street,
+                    City = src.City
+                }));
+
+            CreateMap<Trainer, TrainerViewModel>()  
+                 .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender.ToString()))
+                 .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
+                 .ForMember(dest => dest.Address, opt => opt.MapFrom(src => $"{src.Address.BuildingNumber}, {src.Address.Street}, {src.Address.City}"));
+
+
+            CreateMap<Trainer, TrainerToUpdateViewModel>()
+                .ForMember(dest => dest.BuildingNumber, opt => opt.MapFrom(src => src.Address.BuildingNumber))
+                .ForMember(dest => dest.Street, opt => opt.MapFrom(src => src.Address.Street))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Address.City));
+
+            #endregion
+
         }
     }
 }
