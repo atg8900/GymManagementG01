e98c49492cba14d8bc0375d5827a9b2565984cb2
atg8900 <abdallahtarek888888@gmail.com>
2025-10-19 17:24:44 +0300

    Action Return Type

--- GymManagementPL/Controllers/HomeController.cs
+++ GymManagementPL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
 {
@@ -9,6 +10,34 @@ namespace GymManagementPL.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        //BaseURL/Home/Trainers
+        public JsonResult Trainers()
+        {
+            var trainers = new List<Trainer>() 
+            { 
+                new Trainer(){Name="abdullah" , Phone="0106325600"},
+                new Trainer(){Name="aya" , Phone="0106325656"},
+            };
+            return Json( trainers);
+        }
+
+        public RedirectResult Redirect()
+        {
+            return Redirect("https://www.linkedin.com/");
+        }
+
+        public ContentResult GetContent()
+        {
+            return Content("<h1>Hello From Gold's Gym</h1>","text/html");
+        }
+
+        public FileResult DownloadFile()
+        {
+            var pathFile = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot" ,"css" ,"site.css");
+            var fileBytes = System.IO.File.ReadAllBytes(pathFile);
+            return File(fileBytes , "text/css" ,"AtgStyle.css");
         }
     }
 }
